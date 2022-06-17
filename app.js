6b03479dbeaa556bc35021671236d804a1f07ef7
Nasrullah-balghari <nasrullahbalghari676@gmail.com>
2022-06-17 12:19:33 +0500

    assignment-5

---
+++ app.js
@@ -0,0 +1,121 @@
+//Question-1
+// var mulArray = [[],[],[]];
+
+//Question-2
+// var matrix = [
+//            [0,1,2,3],
+//            [1,0,1,2],
+//            [2,1,0,1]
+//             ];
+// for (var i = 0; i < 3; i++){
+//     for (var j=0; j< 4; j++){
+
+//         document.write(matrix[i][j] + "  "+" ")
+//     }
+//     document.write("<br>")
+// }
+
+//Question-3
+// for (var i = 1; i <= 10; i++){
+//     document.write(i+"<br>")
+// }
+
+//Question-4
+// var table = +prompt("Enter the number which you want to \n generate table");
+// var tableLength = +prompt("Enter the length");
+// for (var i = 0; i <=tableLength; i++) {
+//     document.write("Multiplicationj of "+table+" Length "+tableLength+"<br>");
+//     document.write(table +" X "+ i + "= "+table*i+"<br>")
+// }
+
+//Question-5
+// var fruits = ["apple","banana","mango","orange","strawberry"];
+// for (var i = 0; i<fruits.length; i++) {
+//     document.write(fruits[i] +"<br>")
+//     document.write("<br>")
+// }
+// for (var i=0; i <fruits.length;i++) {
+//     document.write("Element at index "+i+"is "+fruits[i]+"<br>")
+// }
+
+//Question-6
+//a
+// var counting = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// document.write("<h5>"+"Counting:"+"</h5>")
+// for (var i =0; i < counting.length;i++) {
+//     document.write(counting[i]+" ")
+// }
+//b
+// var reverseCounting = [1,2,3,4,5,6,7,8,9,10];
+// var reverse = reverseCounting.reverse();
+// document.write("<h5>"+"Reverse counting:"+"</h5>"+"<br>"+reverse +" ");
+//c
+// var even = [0,2,4,6,8,10,12,14,16,18,20];
+// document.write("<h5>"+"Even: "+"<h5>")
+// for (var i = 0; i < even.length; i++) {
+//     document.write(even[i] +" ");
+// }
+//d
+// var odd = [1,3,5,7,9,11,13,15,17,19];
+// document.write("<h5>"+"Odd: "+"<h5>")
+// for (var i = 0; i < odd.length; i++) {
+//     document.write(odd[i] + " ");
+// }
+//e
+// var series = ["2k","4k","6k","8k","10k","12k","14k","16k","18k","20k"];
+// document.write("<h5>"+"Series: "+"<h5>")
+// for (var i = 0; i < series.length; i++) {
+//     document.write(series[i] + " ");
+// }
+
+//Question-7
+// var cakes = ["cake","apple pie","cookie","chips","patties"]
+// var customerInput = prompt("Welcome to the Bismillah bakery.What do you want to order sir/ma'am")
+// var flag = false;
+// for (var i = 0; i < cakes.length; i++){
+//     if (cakes[i] === customerInput.toLowerCase()){
+//         flag = true;
+//         document.write(customerInput+ " is availabe at index "+i+" in our bakery")
+//     } 
+// }
+// if (flag === false) {
+//     document.write("We are sorry."+customerInput+" not available")
+// }
+
+//Qestion-8
+// var arr = [24,53,78,91,12];
+// var max = 0;
+// for (var i = 0; i < arr.length; i++){
+//     if (arr[i] > max) {
+//         max = arr[i];
+//     }        
+
+//     }
+//     document.write(max);
+
+  //Question-9
+//   var arr = [24,53,78,91,12];
+// var min = arr[0];
+// for (var i = 0; i < arr.length; i++){
+//     if (arr[i] < min) {
+//         min = arr[i];
+//     }        
+
+//     }
+//     document.write(min);
+  
+//Question-10
+// var num = 5;
+// for (var i=5; i<=100; i=i+5){
+//     document.write(i+" ")
+// }
+
+
+
+
+
+
+
+
+
+
